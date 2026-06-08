8d2fbc5ffdbe2b0799040821db48d3a1ef93729e
nikhil2005trx <149887804+nikhil2005trx@users.noreply.github.com>
2026-06-09 01:52:55 +0530

    Fix image filename case

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -2,7 +2,7 @@ import  { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import "./Register.css";
 import InputBox from "../../components/inputbox/InputBox";
-import logo from "../../assets/images/voyawander.png";
+import logo from "../../assets/images/Voyawander.png";
 import Restriction from "../../utils/Restriction";
 import OtpInput from "../../components/inputbox/OtpInput";
 import { useLoading } from "../../context/LoadingContext";
